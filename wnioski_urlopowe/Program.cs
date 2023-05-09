1129e8312b3f89b5b6369478d2ea6e4c8639faf2
Kamil Lach <k.lach@necto.local>
2023-05-09 11:27:06 +0200

    validation methods, days counting, show summary method, choiceAction method

--- wnioski_urlopowe/Program.cs
+++ wnioski_urlopowe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
@@ -6,11 +7,12 @@ namespace Program
 {
     class Data
     {
-        private string name, surname, startDate, endDate;
+        private string name, surname, startDateString, endDateString;
+        private DateTime startDate, endDate;
 
-        public bool nameValidation(string name)
+        private bool nameValidation(string name)
         {
-            string namePattern = @"^[a-zA-Z]+$";
+            string namePattern = @"^[A-Z][a-zA-Z]{1,}$";
             bool isMatchN = Regex.IsMatch(name, namePattern);
             if (!isMatchN)
             {
@@ -21,9 +23,9 @@ namespace Program
                 return true;
         }
 
-        public bool surnameValidation(string surname)
+        private bool surnameValidation(string surname)
         {
-            string namePattern = @"^[a-zA-Z-]+$";
+            string namePattern = @"^[A-Z][a-zA-Z-]{1,}$";
             bool isMatchS = Regex.IsMatch(surname, namePattern);
             if (!isMatchS)
             {
@@ -34,21 +36,36 @@ namespace Program
                 return true;
         }
 
-        public bool dateValidation(string sD, string eD)
+        private bool dateValidation(string sD, string eD)
         {
             string datePattern = @"^\d{2}-\d{2}-\d{4}$";
             bool isMatchsD = Regex.IsMatch(sD, datePattern);
             bool isMatcheD = Regex.IsMatch(eD, datePattern);
-            
-            if(!(isMatcheD && isMatchsD) )
+
+            if (!(isMatcheD && isMatchsD))
             {
                 Console.WriteLine("Wpowadzono nieprawdidłową datę. Oczekiwany format: \"DD-MM-RRRR\"");
                 return false;
             }
 
 
-            DateTime startDate = DateTime.ParseExact(sD, "dd-MM-yyyy", CultureInfo.InvariantCulture);
-            DateTime endDate = DateTime.ParseExact(eD, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            try
+            {
+                startDate = DateTime.ParseExact(sD, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+                endDate = DateTime.ParseExact(eD, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                Console.WriteLine("Data jest nieprawidłowa!");
+                return false;
+            }
+
+
+            if (DateTime.Compare(startDate, DateTime.Now) <= 0)
+            {
+                Console.WriteLine("Wprowadzona data początkowa nie może być przed dzisiejsza datą!");
+                return false;   
+            }
 
             if (startDate <= endDate)
                 return true;
@@ -59,41 +76,84 @@ namespace Program
             }
         }
 
+        private int daysCounting(DateTime startDate, DateTime endDate)
+        {
+            int days = 0;
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if(date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+                    days++;
+            }
+            return days;
+        }
+
        
         public void getData()
         {
             do
-     
-            Console.WriteLine("Podaj imię: ");
-            name = Console.ReadLine();
+            {
+                Console.WriteLine("Podaj imię: ");
+                name = Console.ReadLine();
 
-            Console.WriteLine("Podaj nazwisko: ");
-            surname = Console.ReadLine();
+            } while (!nameValidation(name));
 
             do
             {
-                Console.WriteLine("Podaj date początkową urlopu: ");
-                startDate = Console.ReadLine();
+                Console.WriteLine("Podaj nazwisko: ");
+                surname = Console.ReadLine();
 
-                Console.WriteLine("Podaj date końcową urlopu: ");
-                endDate = Console.ReadLine();
-            } while(!dateValidation(startDate, endDate));
+            } while(!surnameValidation(surname));
 
+            do
+            {
+                Console.WriteLine("Podaj date początkową urlopu (\"DD-MM-RRRR\"): ");
+                startDateString = Console.ReadLine();
+
+                Console.WriteLine("Podaj date końcową urlopu (\"DD-MM-RRRR\"): ");
+                endDateString = Console.ReadLine();
+
+            } while(!dateValidation(startDateString, endDateString));
             
         }
-        public void printData()
-        {
 
+        public void showSummary()
+        {
+            int days = daysCounting(startDate, endDate);
+            Console.WriteLine($"Przyjęto wniosek urlopowy dla {name} {surname} od {endDateString} do {endDateString}");
+            Console.WriteLine($"Ilość dni: {days}");
         }
     }
     class Menu
     {
+        static string choice;
+
+        static void choiceAction(Data dataObject)
+        {
+           
+            bool validChoice = false;
+            while (!validChoice)
+            {
+                Console.WriteLine("Wybierz: ");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        validChoice = true;
+                        dataObject.getData();
+                        dataObject.showSummary();
+                        break;
+                    default:
+                        Console.WriteLine("Nieprawidłowy wybór!");
+                        break;
+                }
+            }
+        }
+        
         static void Main()
         {
-            Console.WriteLine("Nowy wniosek urlopowy");
             Data data = new Data();
-            data.getData();
-
+            Console.WriteLine("1. Nowy wniosek urlopowy");
+            choiceAction(data);
         }
     }
 }
